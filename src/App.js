8239ed192f8a37576fcefd271e3d81786841320d
1342470 <1342470@stu.ucenmanchester.ac.uk>
2022-01-31 23:35:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import logo from './res/1_AJk55yyiWpZfDHlS2BPj9g.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Flashcards</h1>
+
       </header>
-    </div>
+
+ </div>
   );
 }
 
